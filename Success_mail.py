fec18baf485fb4be197d3db39548c017520719a9
kajal Gayakwad <77723209+kajal2499@users.noreply.github.com>
2022-03-20 21:14:58 +0530

    Update Success_mail.py

--- Success_mail.py
+++ Success_mail.py
@@ -7,7 +7,7 @@ acc*=100
 file.close()
 host_address = "kajalgayakwad99@gmail.com"
 host_pass = "ycdqqcrztgfnbdid"
-guest_address = "clanofking001@gmaiul.com"
+guest_address = "clanofking001@gmail.com"
 subject = "Regarding Success of your model "
 content = '''Dear Admin, 
 				Your trained model has given best accuracy .
